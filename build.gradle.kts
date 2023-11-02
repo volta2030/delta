a35c3a1e854781ff413227470c83530c9195344f
volta2030 <volta2030@gmail.com>
2023-11-02 22:14:13 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.snacklab"
+            artifactId = "delta"
+            version = "1.0.0"
+
+            from(components["java"])
+        }
+    }
+}
---
+++ src/test/kotlin/Main.kt
@@ -0,0 +1,3 @@
+fun main(args: Array<String>) {
+    println(delta.Cipher.decodePackBits(byteArrayOf(1,2,3,4,5,6,7,8,9)))
+}
