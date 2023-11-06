26107629866ba31deb1c601f5659fbede554b141
volta2030 <volta2030@gmail.com>
2023-11-06 22:15:04 +0900

    -Add Cipher Interface

--- src/main/kotlin/delta/Cipher.kt
+++ src/main/kotlin/delta/Cipher.kt
@@ -1,29 +1,6 @@
 package delta
 
-class Cipher {
-    companion object{
-        fun decodePackBits(byteArray: ByteArray) : ByteArray{
-            var decryption = byteArrayOf()
-
-            var i = 0
-            while(i < byteArray.size){
-                when (val integer = byteArray[i].toInt()) {
-                    -128 -> {
-                        i = byteArray.size
-                    }
-                    in 0..<128 -> {
-                        decryption += byteArray.sliceArray(i+ 1..<i + 1 + (integer + 1))
-                        i += integer + 2
-                    }
-                    in -127..<0 -> {
-                        for(j : Int in 0..<-integer + 1){
-                            decryption += byteArray[i + 1]
-                        }
-                        i += 2
-                    }
-                }
-            }
-            return decryption
-        }
-    }
+interface Cipher {
+    fun encode(byteArray: ByteArray) : ByteArray
+    fun decode(byteArray: ByteArray) : ByteArray
 }
