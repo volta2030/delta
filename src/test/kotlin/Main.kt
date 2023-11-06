2d1a2fe7cf905e428ecf72213489ed71daa7f819
volta2030 <volta2030@gmail.com>
2023-11-06 22:15:41 +0900

    -Modify Main Class...

--- src/test/kotlin/Main.kt
+++ src/test/kotlin/Main.kt
@@ -1,3 +1,3 @@
 fun main(args: Array<String>) {
-    println(delta.Cipher.decodePackBits(byteArrayOf(1,2,3,4,5,6,7,8,9)))
+    println(delta.Packbits.decodePackBits(byteArrayOf(1,2,3,4,5,6,7,8,9)))
 }
